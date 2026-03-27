c039758eef96f76a083ac1d9a0bf96ce27c43290
Dekel Pilli <dekelpilli@gmail.com>
2026-03-28 00:19:56 +1100

    Add missing script, add CI for deleting old deployments

---
+++ scripts/generate-content.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+REPO_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
+cd "$REPO_ROOT"
+
+# Set updated time
+cat sns/meta-sns.json | jq "._meta.dateLastModified = $(date +'%s')" > sns/meta-sns.json
+
+# Generate merged homebrew JSON
+bb .github/generate-merged-json.bb
+
+# Prepare SNS data
+cat homebrew/sns.json | jq -c '.spell' > data/spells/spells-sns.json
+cat homebrew/sns.json | jq -c '.monster' > data/bestiary/bestiary-sns.json
+cat homebrew/sns.json | jq -c '{class,subclass,classFeature,subclassFeature}' > data/class/class-sns.json
+
+# Prepare search indexes
+mkdir -p search
+npm install
+npm run gen:search-index
+npm run gen:pages
+
+npm run build:sw:prod
