ff75d395e0df0f4d873497fb6b9045404da1af13
Dekel Pilli <dekelpilli@gmail.com>
2026-03-28 21:47:32 +1100

    Further cleanup

--- scripts/generate-content.sh
+++ scripts/generate-content.sh
@@ -4,8 +4,15 @@ set -euo pipefail
 REPO_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 cd "$REPO_ROOT"
 
+jq_inplace() {
+	local file="$1"; shift
+	local tmp
+	tmp=$(mktemp)
+	jq "$@" "$file" > "$tmp" && mv "$tmp" "$file"
+}
+
 # Set updated time
-cat sns/meta-sns.json | jq "._meta.dateLastModified = $(date +'%s')" > sns/meta-sns.json
+jq_inplace sns/meta-sns.json "._meta.dateLastModified = $(date +'%s')"
 
 # Generate merged homebrew JSON
 bb .github/generate-merged-json.bb
@@ -13,12 +20,15 @@ bb .github/generate-merged-json.bb
 # Prepare SNS data
 cat homebrew/sns.json | jq -c '{spell}' > data/spells/spells-sns.json
 cat homebrew/sns.json | jq -c '{monster}' > data/bestiary/bestiary-sns.json
-cat homebrew/sns.json | jq -c '{optionalfeature}' > data/optionalfeatures.json
 cat homebrew/sns.json | jq -c '{class,subclass,classFeature,subclassFeature}' > data/class/class-sns.json
 
-# Prepare search indexes
+cat homebrew/sns.json | jq -c '{optionalfeature}' > data/optionalfeatures.json
+jq_inplace homebrew/sns.json -c 'del(.optionalfeature)'
+
+# Generate all relevant indexes and pages
 mkdir -p search
 npm install
+mkdir -p data/book
 npm run gen
 
 npm run build:sw:prod
